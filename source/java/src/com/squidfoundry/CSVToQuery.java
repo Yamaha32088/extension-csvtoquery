22d78175441a17ba120362db0141769e729c5b25
Brandon Brown <Brandon@encoremp.com>
2018-06-06 07:41:12 -0500

    Default first row (#5)
    * default trim and first row header to true
    
    * fixes #4

--- source/java/src/com/squidfoundry/CSVToQuery.java
+++ source/java/src/com/squidfoundry/CSVToQuery.java
@@ -34,15 +34,15 @@ public final class CSVToQuery extends BIF implements Function {
 	private static boolean csvIsFile;
 
 	public static Object call(PageContext pc, String csv) throws PageException, IOException {
-		return _call(pc, csv, "", false, false, null);
+		return _call(pc, csv, "", true, true, null);
 	}
 	
 	public static Object call(PageContext pc, String csv, String filePath) throws PageException, IOException {
-		return _call(pc, csv, filePath, false, false, null);
+		return _call(pc, csv, filePath, true, true, null);
 	}
 
 	public static Object call(PageContext pc, String csv, String filePath, Boolean firstRowIsHeader) throws PageException, IOException {
-		return _call(pc, csv, filePath, firstRowIsHeader, null, null);
+		return _call(pc, csv, filePath, firstRowIsHeader, true, null);
 	}
 	
 	public static Object call(PageContext pc, String csv, String filePath, Boolean firstRowIsHeader, Boolean trim) throws PageException, IOException {
@@ -138,7 +138,7 @@ public final class CSVToQuery extends BIF implements Function {
 	
 	private static CSVFormat getCSVFormat(Character delimiter) {
 		CSVFormat format = CSVFormat.RFC4180;
-		format = format.withIgnoreSurroundingSpaces();
+		format = format.withIgnoreSurroundingSpaces().withIgnoreEmptyLines(true);
 		format = (delimiter != null) ? format.withDelimiter(delimiter) : format;
 		return format;
 	}
